8982e2c21a6e2ab987b45dcd5b0825f6cc1ded16
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-01-25 23:09:12 +0330

    Improve Code Quality

--- OnlineSchoolPredictor/buttons.py
+++ OnlineSchoolPredictor/buttons.py
@@ -1,7 +1,4 @@
 from bale import Keyboard, Components, InlineKeyboard
 MENU_BUTTONS = Components(keyboards = [Keyboard("وضیعت تعطیلی تهران 📋")])
 ACCOUNT_MENU = Components(inline_keyboards = [InlineKeyboard("ثبت نام", callback_data = "register")])
-def Vote_buttons(yes, no):
-    return Components(
-        inline_keyboards = [InlineKeyboard(f"فردا تعطيل است {yes}", callback_data="yes"), InlineKeyboard(f"فردا تعطيل نيست {no}", callback_data="no")]
-    )
+Vote_buttons = lambda tatil_vote, baz_vote: return Components(inline_keyboards = [InlineKeyboard(f"فردا تعطيل است {tatil_vote}", callback_data="yes"), InlineKeyboard(f"فردا تعطيل نيست {baz_vote}", callback_data="no")])
